bad8c1ebde35c5b6816823bba47b3212b408d845
KarasinskiKacper <karasinski.kacper@outlook.com>
2025-03-12 17:47:53 +0100

    zaimplementowanie preprocesora sass

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,13 @@
 from flask import Flask, request, render_template
+from flask_assets import Environment, Bundle
 
 app = Flask(__name__, template_folder = '../frontend')
+
+assets = Environment(app)
+app.static_folder = '../frontend/static'
+assets.url = app.static_url_path
+sass = Bundle('sass/home.sass', filters=['libsass'], output='all.css')
+assets.register('sass_all', sass)
 
 @app.route('/', methods=['GET',"POST"])
 def hello():
