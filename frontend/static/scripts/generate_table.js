e0fe7a51af3d86a75e36ab1e7be3afab412eb73f
Paweł Dyczek <paweldyczek02@gmail.com>
2025-03-20 20:51:33 +0100

    poprawa nazwy zmiennej

--- frontend/static/scripts/generate_table.js
+++ frontend/static/scripts/generate_table.js
@@ -10,13 +10,13 @@ function generateHourColumn() {
     "week-layout__grid-hour-column"
   ).length;
 
-  let hourColumn = document.getElementsByClassName(
+  let hour_column = document.getElementsByClassName(
     "week-layout__grid-hour-column"
   )[indx - 1];
 
   // Generowanie godzin w kolumnie
   for (let i = 1; i < 24; i++) {
-    hourColumn.innerHTML += `
+    hour_column.innerHTML += `
           <p class=" week-layout__grid-hour numeric-font">
           ${i.toString().padStart(2, "0")}:00
           </p>`;
