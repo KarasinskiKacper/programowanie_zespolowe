e36a081d1a44f463e1f3c7b056b70fc3e220a3af
Qler-gra <karasinski.kacper@outlook.com>
2025-03-21 18:05:45 +0100

    dodanie opisu gdzie należy podpiąć bazę danych

--- frontend/static/scripts/generate_main_calendar.js
+++ frontend/static/scripts/generate_main_calendar.js
@@ -1,4 +1,5 @@
 import { generate_calendar } from "./generate_calendar.js";
+
 export function generate_main_calendar(date) {
   const current_date = new Date(Date.now());
   const is_current_month = current_date.getMonth() === date.getMonth();
@@ -11,8 +12,36 @@ export function generate_main_calendar(date) {
   let counter = 1;
   let is_past_month = true;
 
+  // pętla generująca html dla głównego kalendarza
   calendar.forEach((week) => {
     week.forEach((day) => {
+      // TODO poniżej należy przekazać z bazy danych informacje dniach w których występuje zadanie oraz jego nazwę
+      // day - numer dnia
+      // przykład, miejsca do usupełnienia zanaczone: <<>>
+      /*
+      if (day === counter) {
+        new_innerhtml += `<div class="month__main-calendar-day">
+            <p
+              class="month__main-calendar-days-text ${
+                is_current_month &&
+                day === current_date.getDate() &&
+                "month__main-calendar-days-text--today"
+              } ${
+                << wyrażenie sprawdzające czy w tym dniu jest zadanie np: day === zmienna_z_numerem_dnia_zadania >> &&
+                "month__main-calendar-days-text--task"
+              }"
+            >
+              ${day}
+            </p>
+            <p class="month__main-calendar-days-task">
+              << tytuł/y zadania/zadań >>
+            </p>
+          </div>`;
+        counter++;
+      } 
+      */
+
+      // html dla dni aktualnie wyświetlanego miesiąca
       if (day === counter) {
         new_innerhtml += `<div class="month__main-calendar-day">
             <p
@@ -24,10 +53,12 @@ export function generate_main_calendar(date) {
             >
               ${day}
             </p>
-            <div class="month__main-calendar-days-task"></div>
+            <p class="month__main-calendar-days-task"></p>
           </div>`;
         counter++;
-      } else {
+      }
+      // html dla dni z końcówki poprzedniego i początku następnego miesiąca (w stosunku do akutualnie wyświetlanego miesiąca)
+      else {
         new_innerhtml += `<div class="month__main-calendar-day">
             <p
               class="month__main-calendar-days-text month__main-calendar-days-text--${
@@ -36,7 +67,7 @@ export function generate_main_calendar(date) {
             >
               ${day}
             </p>
-            <div class="month__main-calendar-days-task"></div>
+            <p class="month__main-calendar-days-task"></p>
           </div>`;
       }
     });
