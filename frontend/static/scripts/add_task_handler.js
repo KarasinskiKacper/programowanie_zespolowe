fd9cb2d60894a4d1b461c625a7b4a90ec1333815
KarasinskiKacper <karasinski.kacper@outlook.com>
2025-03-24 15:43:06 +0100

    dodanie wyłączania inputów po kliknięciu checkboxa "cały dzień"

--- frontend/static/scripts/add_task_handler.js
+++ frontend/static/scripts/add_task_handler.js
@@ -1,7 +1,22 @@
+// przypisanie do zmiennej: wrappera (tła), checkboxa i wyłączalnych inputów
 const addTaksWrapper = document.querySelector(".add-task__wrapper");
+const allDayCheckbox = document.querySelector(".add-task__all_day_check");
+const disableableInputs = document.querySelectorAll(
+  ".add-task__input--disableable"
+);
 
+// listener chowający popup po kliknięciu poza jego obszar
 addTaksWrapper.addEventListener("click", (e) => {
   if (e.target.className === "add-task__wrapper") {
     addTaksWrapper.classList.add("add-task__wrapper--hidden");
+  }
+});
+
+// listener przełączający inputy po kliknięciu checkboxa
+allDayCheckbox.addEventListener("change", () => {
+  if (allDayCheckbox.checked) {
+    disableableInputs.forEach((input) => (input.disabled = true));
+  } else {
+    disableableInputs.forEach((input) => (input.disabled = false));
   }
 });
